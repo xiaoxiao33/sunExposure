0d7d655b71cbf18ceff2c8452af88059a0796896
Xiaoxiao Ouyang <xiaoxiaoglobe33@gmail.com>
2018-02-22 16:14:05 -0600

    all files

---
+++ app/src/main/java/com/example/xiaoxiaoouyang/sunexposure/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.xiaoxiaoouyang.sunexposure;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToMenu(View view) {
+        Intent intent = new Intent(this, Menu.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/xiaoxiaoouyang/sunexposure/Menu.java
@@ -0,0 +1,16 @@
+package com.example.xiaoxiaoouyang.sunexposure;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+//        Intent intent = getIntent();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
