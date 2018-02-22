55dbea7e29f93224c49cce8e601c9f9ef2e75c6e
Xiaoxiao Ouyang <xiaoxiaoglobe33@gmail.com>
2018-02-22 16:47:24 -0600

    modified menu page

--- app/src/main/java/com/example/xiaoxiaoouyang/sunexposure/Menu.java
+++ app/src/main/java/com/example/xiaoxiaoouyang/sunexposure/Menu.java
@@ -3,14 +3,24 @@ package com.example.xiaoxiaoouyang.sunexposure;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-//        Intent intent = getIntent();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void goToDiagram(View view) {
+        Intent intent = new Intent(this, ExposureDiagram.class);
+        startActivity(intent);
+    }
+
+    public void goToQuestion(View view) {
+        Intent intent = new Intent(this, Questions.class);
+        startActivity(intent);
     }
 }
