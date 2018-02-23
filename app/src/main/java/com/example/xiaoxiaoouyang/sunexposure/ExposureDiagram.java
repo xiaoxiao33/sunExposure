327f8f774864e22ee9e9fdce60f546248fdae549
Xiaoxiao Ouyang <xiaoxiaoglobe33@gmail.com>
2018-02-22 20:25:55 -0600

    Added different views on Diagram activity

--- app/src/main/java/com/example/xiaoxiaoouyang/sunexposure/ExposureDiagram.java
+++ app/src/main/java/com/example/xiaoxiaoouyang/sunexposure/ExposureDiagram.java
@@ -1,7 +1,11 @@
 package com.example.xiaoxiaoouyang.sunexposure;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ExposureDiagram extends AppCompatActivity {
 
@@ -9,5 +13,40 @@ public class ExposureDiagram extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exposure_diagram);
+    }
+
+    public void FragmentOneClick(View view) {
+        Fragment myfragment;
+        myfragment = new FragmentOne();
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_switch, myfragment);
+        fragmentTransaction.commit();
+
+    }
+    public void FragmentTwoClick(View view) {
+        Fragment myfragment;
+        myfragment = new FragmentTwo();
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_switch, myfragment);
+        fragmentTransaction.commit();
+
+    }
+
+    public void FragmentThreeClick(View view) {
+        Fragment myfragment;
+        myfragment = new FragmentThree();
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_switch, myfragment);
+        fragmentTransaction.commit();
+
+    }
+    public void goBack(View view) {
+        super.onBackPressed();
     }
 }
---
+++ app/src/main/java/com/example/xiaoxiaoouyang/sunexposure/FragmentOne.java
@@ -0,0 +1,17 @@
+package com.example.xiaoxiaoouyang.sunexposure;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentOne extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_one, container, false);
+
+    }
+}
