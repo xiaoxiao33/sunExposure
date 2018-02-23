e9742524ea03bab5ec1d9104a311f417b6a17098
iancaoo <33076510+iancaoo@users.noreply.github.com>
2018-02-22 23:42:22 -0600

    point_diagram

--- app/src/main/java/com/example/xiaoxiaoouyang/sunexposure/ExposureDiagram.java
+++ app/src/main/java/com/example/xiaoxiaoouyang/sunexposure/ExposureDiagram.java
@@ -3,6 +3,11 @@ package com.example.xiaoxiaoouyang.sunexposure;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.Series;
+
 public class ExposureDiagram extends AppCompatActivity {
 
     @Override
@@ -10,4 +15,13 @@ public class ExposureDiagram extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exposure_diagram);
     }
+    GraphView graph = (GraphView) findViewById(R.id.graph);
+    LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+            new DataPoint(0, 1),
+            new DataPoint(1, 5),
+            new DataPoint(2, 3)
+    });
+    graph.addSeries(series);
+
+
 }
